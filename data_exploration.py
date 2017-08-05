8ef48c9f44b874c415bb0f80b4c91e2a17fc0959
Antonin Barthélémy <antobarth@gmail.com>
2017-08-05 08:45:23 +0200

    update : changement de hierachie

--- data_exploration.py
+++ data_exploration.py
@@ -1,5 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+'''Testing File.'''
+# pylint: disable=C0103
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
 import script.CustomFunction as cf
 from sklearn.model_selection import train_test_split
 from sklearn import svm
@@ -11,15 +14,15 @@ df_train = cf.sample_data_row_ananas(df_train.iloc[:, 2:], 5, 10)
 
 X_train, X_test, y_train, y_test = train_test_split(df_train, test_size=0.2)
 
-print(train.iloc[:, -1:].values.flatten())
+# print(train.iloc[:, -1:].values.flatten())
 # train.info()
 # print(train.head())
 # print(train.iloc[:10, :], train.iloc[:10, -1:])
 clf = svm.SVR()
-clf.fit(train.iloc[:, :-1].values, train.iloc[:, -1:].values.flatten())
+clf.fit(X_train.iloc[:, :-1].values, X_train.iloc[:, -1:].values.flatten())
 
-y_pred = clf.predict(test.iloc[:, :-1])
+y_pred = clf.predict(y_test.iloc[:, :-1])
 
-report = explained_variance_score(test.iloc[:, -1:], y_pred)
+report = explained_variance_score(y_test.iloc[:, -1:], y_pred)
 
 print('Score : {0}'.format(report))
